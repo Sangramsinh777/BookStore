a3d9b89769706310180715d61e77c16c496f966c
bhatesariya844 <bhatesariya844@gmail.com>
2020-12-17 22:11:58 +0530

    MyBookStore Application

---
+++ MyBooksStore/MyBooksStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyBooksStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
